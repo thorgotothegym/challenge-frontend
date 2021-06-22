62caf4d57b680bf69dd51dd8eb694be032a303c5
BePandaMyFriend <al3ix.vidal@gmail.com>
2021-06-22 18:30:42 +0200

    feat: start form

--- src/containers/sensors/Sensors.tsx
+++ src/containers/sensors/Sensors.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Button } from "antd";
+import { Modal, Button, Input, Form } from "antd";
 import { AxiosError, AxiosResponse } from "axios";
 
 import { instance } from "../../request/http";
@@ -11,6 +11,15 @@ import styled from "styled-components";
 export const Sensors = () => {
   const [sensors, setSensors] = useState<ISensor[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
+
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  const [createSensor, setCreatedSensor] = useState<ISensor>({
+    description: "",
+    id: 0,
+    isActive: false,
+    samplingPeriod: 0,
+  });
 
   const getSensors = async () => {
     instance
@@ -24,10 +33,30 @@ export const Sensors = () => {
       });
   };
 
-  /* useEffect(() => {
-    
-    getSensors();
-  }, []); */
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+
+  const handleOk = () => {
+    setIsModalVisible(false);
+    console.log("createsendor", createSensor);
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
+
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(`checked = ${event.target.checked}`);
+  };
+
+  const onFinish = (values: any) => {
+    console.log("Success:", values);
+  };
+
+  const onFinishFailed = (errorInfo: any) => {
+    console.log("Failed:", errorInfo);
+  };
 
   return (
     <>
@@ -41,14 +70,29 @@ export const Sensors = () => {
         >
           Get all the sensors
         </Button>
-        <Button type="primary" disabled={true}>Creates a sensor</Button>
-        <Button type="primary" disabled={true}>Get senor_id sensor data</Button>
-        <Button type="primary" disabled={true}>Replaces senor_id sensor data</Button>
-        <Button type="primary" disabled={true}>Partially updates senor_id sensor data</Button>
-        <Button type="primary" disabled={true}>Deletes the senor_id sensor</Button>
+        <Button
+          type="primary"
+          onClick={() => {
+            showModal();
+          }}
+        >
+          Creates a sensor
+        </Button>
+        <Button type="primary" disabled={true}>
+          Get senor_id sensor data
+        </Button>
+        <Button type="primary" disabled={true}>
+          Replaces senor_id sensor data
+        </Button>
+        <Button type="primary" disabled={true}>
+          Partially updates senor_id sensor data
+        </Button>
+        <Button type="primary" disabled={true}>
+          Deletes the senor_id sensor
+        </Button>
       </WrapperOptions>
-      
-       {loading === true ? (
+
+      {loading === true ? (
         "loading"
       ) : (
         <div>
@@ -58,12 +102,39 @@ export const Sensors = () => {
                 <ul>
                   <li>ID: {item.id}</li>
                   <li>Description: {item.description}</li>
-                  <li>isActive: {item.isActive === true ? 'True' : 'False'}</li>
+                  <li>isActive: {item.isActive === true ? "True" : "False"}</li>
                   <li>SampligPeriod: {item.samplingPeriod}</li>
                 </ul>
               </div>
             );
           })}
+          <div>
+            <Modal
+              title="Basic Modal"
+              visible={isModalVisible}
+              onOk={handleOk}
+              onCancel={handleCancel}
+            >
+              <Form
+                name="basic"
+                onFinish={onFinish}
+                onFinishFailed={onFinishFailed}
+              >
+                <Input name="id" placeholder="id" type="number" />
+                <Input
+                  name="description"
+                  placeholder="description"
+                  onChange={ () => {}}
+                  type="text"
+                />
+                <Input
+                  name="samplingPeriod"
+                  placeholder="samplingPeriod"
+                  type="number"
+                />
+              </Form>
+            </Modal>
+          </div>
         </div>
       )}
     </>
